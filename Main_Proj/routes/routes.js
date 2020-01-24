156f67521a8e1fc3f7eccbeba8e73869faf632d3
propene123 <benjamin.russell@durham.ac.uk>
2020-01-24 00:47:42 +0000

    file should actually be uploaded now

--- Main_Proj/routes/routes.js
+++ Main_Proj/routes/routes.js
@@ -2,13 +2,15 @@
 
 const router = require('express').Router();
 const keys = require('../config/keys');
-let bodyParser = require('body-parser');
-let multer = require('multer');
-let upload = multer({dest:'local/uploads/'});
+const bodyParser = require('body-parser');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(keys.google.clientID);
 const mysql = require('mysql');
-const { DBHandler } = require('../libs/DBHandler')
+const { DBHandler } = require('../libs/DBHandler');
+const multer = require('multer');
+const upload = multer({dest:'photos/tmp'});
+const fs = require('fs');
+
 
 
 async function verify(token) {
@@ -21,6 +23,8 @@ async function verify(token) {
 }
 router.use(bodyParser.urlencoded({ extended: false }));
 
+
+// This function is for getting the list of exisiting photos not the individual photos
 router.post('/user/getPhotos', async function(req, res) {
     res.type('json');
     let body = '';
@@ -32,7 +36,7 @@ router.post('/user/getPhotos', async function(req, res) {
         let db = new DBHandler(keys.mysql.host, keys.mysql.user, keys.mysql.password, keys.mysql.database);
         let resp  = await db.connect();
         if (resp){
-            resp = await db.query('SELECT `PhotoPath` FROM `PassportPage` WHERE `UserID` = ' + mysql.escape(user['sub']));
+            resp = await db.query('SELECT `SculptureID` FROM `PassportPage` WHERE `UserID` = ' + mysql.escape(user['sub']));
             if(resp){
                 res.status(200);
                 body = resp;
@@ -52,7 +56,7 @@ router.post('/user/getPhotos', async function(req, res) {
 });
 
 
-router.post('/user/addPhoto', async function(req, res) {
+router.post('/user/addPhoto', upload.single('picture'), async function(req, res) {
     res.type('json');
     let body = '';
     let verif = true;
@@ -60,25 +64,49 @@ router.post('/user/addPhoto', async function(req, res) {
         verif = false;
     });
     if (verif) {
-        let db = new DBHandler(keys.mysql.host, keys.mysql.user, keys.mysql.password, keys.mysql.database);
-        let resp  = await db.connect();
-        if (resp){
-            let userID = user['sub'];
-            let sculptureID = req.body.sculptureID;
-            let photoPath = userID+'/'+sculptureID+'/';
-            let sql = 'INSERT INTO PassportPage (UserID, SculptureID, PhotoPath) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE PhotoPath = ?';
-            sql = mysql.format(sql, [userID, sculptureID, photoPath, photoPath]);
-            resp = await db.query(sql);
-            if(resp){
-                res.status(200);
-                body = resp;
-            } else{
-                res.status(500);
-                body = 'Could not complete query';
+        let file = '';
+        if (req.file) {
+            if (req.file.mimetype == 'image/jpeg' || req.file.mimetype == 'image/png') {
+                file = '/photos/tmp/'+req.file.filename;
+                let movFile = false;
+                try {
+                    fs.renameSync(file, '/photos/'+user['sub']+'/'+req.body.sculptureID+'/1');
+                    movFile = true;
+                } catch (error) {
+                    movFile = false;
+                }
+                if(movFile){
+                    let db = new DBHandler(keys.mysql.host, keys.mysql.user, keys.mysql.password, keys.mysql.database);
+                    let resp  = await db.connect();
+                    if (resp){
+                        let userID = user['sub'];
+                        let sculptureID = req.body.sculptureID;
+                        let photoPath = userID+'/'+sculptureID+'/';
+                        let sql = 'INSERT INTO PassportPage (UserID, SculptureID, PhotoPath) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE PhotoPath = ?';
+                        sql = mysql.format(sql, [userID, sculptureID, photoPath, photoPath]);
+                        resp = await db.query(sql);
+                        if(resp){
+                            res.status(200);
+                            body = resp;
+                        } else{
+                            res.status(500);
+                            body = 'Could not complete query';
+                        }
+                    }else {
+                        res.status(500);
+                        body = 'Could not connect to database';
+                    }
+                }else {
+                    res.status(500);
+                    body = 'Cpuld not store file on the system';
+                }
+            }else {
+                res.status(400);
+                body = 'Uploaded file was not the correct format. PNG or JPEG';
             }
         }else {
-            res.status(500);
-            body = 'Could not connect to database';
+            res.status(400);
+            body = 'No image was sent in the request'
         }
     }else{
         res.status(401);
