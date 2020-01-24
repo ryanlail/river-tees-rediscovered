fc07bc1f3e503e3028a62ef372b83cf77842bbc9
HackaPhil <44399297+HackaPhil@users.noreply.github.com>
2020-01-24 09:08:01 +0000

    Update index.js

---
+++ Main_Proj/client/index.js
@@ -0,0 +1,23 @@
+var pageIndex = 1;
+showPages(pageIndex);
+
+// Next/previous controls
+function plusPages(n) {
+  showPages(pageIndex += n);
+}
+
+// For page skip features
+function currentPage(n) {
+  showPages(pageIndex = n);
+}
+
+function showPages(n) {
+  var i;
+  var pages = document.getElementsByClassName("page");
+  if (n > pages.length) {pageIndex = 1} 
+  if (n < 1) {pageIndex = pages.length}
+  for (i = 0; i < pages.length; i++) {
+      pages[i].style.display = "none"; 
+  }
+  pages[pageIndex-1].style.display = "block"; 
+}
