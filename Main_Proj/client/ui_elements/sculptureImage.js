f5a0951c6423562d88df5ee9a13a3c80c2300dc2
propene123 <benjamin.russell@durham.ac.uk>
2020-01-28 15:01:28 +0000

    added class for managing sculpture image ui element

---
+++ Main_Proj/client/ui_elements/sculptureImage.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class SculptureImage{
+    constructor(src, element, endpoint){
+        this._src = src;
+        this._element = element; 
+        this._endpoint = endpoint;
+    }
+
+
+    refreshStatic(src){
+        this._src = src;
+        this._element.src = this._src;
+    }
+
+    async refreshDatabase(idToken, sculptureID){
+        let api = true;
+        let success = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'idToken='+idToken+'&sculptureID='+sculptureID
+        }).catch(() =>{
+            alert('Could not connect to the server');
+            api = false;
+        });
+        if(!api) return;
+        if(success.ok) {
+            let fileBlob = await success.blob();
+            let fileURL = URL.createObjectURL(fileBlob);
+            this._src = fileURL;
+            this._element.src = this._src;
+        }else {
+            let successMessage = await success.json();
+            alert(successMessage.data);
+        }
+    }
+}
