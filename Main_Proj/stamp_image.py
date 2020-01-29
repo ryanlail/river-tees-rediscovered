b4df8d20675344c4c9662f68a8e425e1a3a35f54
Ryan <rlail1607@gmail.com>
2020-01-29 11:36:38 +0000

    stamping image function

---
+++ Main_Proj/stamp_image.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import sys
+import os
+
+def watermark_image(image_location, stamp_location, stamp_mask_location, file_name):
+
+    image_directory = "photos/tmp/"
+    stamp_directory = "photos/stamps/"
+
+    img = cv2.imread(image_directory + image_location, cv2.IMREAD_COLOR);
+    mask = cv2.imread(stamp_directory + stamp_mask_location, cv2.IMREAD_GRAYSCALE)
+    du_logo = cv2.imread(stamp_directory + stamp_location, cv2.IMREAD_COLOR)
+
+    if not img is None:
+
+        rows, cols, channgels = du_logo.shape
+        #  size of region of interest extracted from image
+        roi = img[0:rows, 0:cols]
+
+        #  black out region of logo in roi
+        img_bg = cv2.bitwise_and(roi, roi, mask = mask)
+
+        # isolate the purple bit (remove white) with inverse mask (so bg is black and removed)
+        logo_fg = cv2.bitwise_and(du_logo, du_logo, mask = cv2.bitwise_not(mask))
+
+
+        # add images together and put back in original image
+        dst = cv2.add(img_bg, logo_fg)
+        img[0:rows, 0:cols] = dst
+
+
+        # write inverted image to file
+
+        resized = cv2.resize(img, (100,100), interpolation = cv2.INTER_AREA)
+
+        cv2.imwrite(file_name + ".png", resized);
+        os.rename(file_name + ".png", file_name)
+
+    else:
+        print("No image file successfully loaded.")
+
+if __name__ == "__main__":
+    original_image_name = sys.argv[1] # just image name
+   # stamp_name = sys.argv[2]
+   # stamp_mask_name = sys.argv[3]
+    file_name = sys.argv[2] # photos/user_id/sculpture_id/1
+
+    watermark_image(original_image_name, "duLogo.png", "logoMask.png", file_name)
