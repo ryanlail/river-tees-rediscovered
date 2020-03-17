b3aba20bf3431464d6c5af8f5793e610d3dbd3a7
propene123 <benjamin.russell@durham.ac.uk>
2020-03-17 19:41:03 +0000

    photo done

--- Main_Proj/client/sculpture.js
+++ Main_Proj/client/sculpture.js
@@ -7,43 +7,60 @@ if(!isNaN(sculptID) && sculptID !== undefined && sculptID != null){
 }
 
 async function initArtist(artistID){
-  let artData = (await(await fetch('/getArtist?artistID='+artistID)).json().catch(() => {
-  return;
-})).data;
-  document.getElementById('artistName').innerHTML = artData[0].Forename + ' ' + artData[0].Surname;
+  try{
+    let resp = await fetch('/getArtist?artistID='+artistID);
+    if (!resp.ok) return;
+    let artData = (await resp.json()).data;
+    if (artData === undefined) return;
+    document.getElementById('artistName').innerHTML = artData[0].Forename + ' ' + artData[0].Surname;
+  } catch(err) {
+    return;
+  }
 }
 
 
 async function initTrailInfo(trailID){
-  let trailData = (await(await fetch('/getTrail?trailID='+trailID)).json().catch(() => {
-  return;
-})).data;
-  document.getElementById('trail').innerHTML = 'Located on the ' + trailData[0].Name + ' trail';
+  try{
+    let resp = await fetch('/getTrail?trailID='+trailID);
+    if (!res.ok) return;
+    let trailData = (await resp.json()).data
+    if (trailData === undefined) return;
+    document.getElementById('trail').innerHTML = 'Located on the ' + trailData[0].Name + ' trail';
+  }catch (err){
+    return;
+  }
 }
 
 
 async function initMapInfo(trailID){
-  let coords = (await(await fetch('/getCoords?trailID='+trailID)).json().catch(() => {
-  return;
-})).data;
-  document.getElementById('map').setAttribute('src', 'https://www.google.com/maps/d/u/2/embed?mid=1SNrHTWqSeCuKSpkuN2XsP4Ol4rUonUD1&z=11&ll=' + coords);
-
+  try{
+    let resp = await fetch('/getCoords?trailID='+trailID);
+    if (!resp.ok) return;
+    let coords = (await resp.json()).data;
+    document.getElementById('map').setAttribute('src', 'https://www.google.com/maps/d/u/2/embed?mid=1SNrHTWqSeCuKSpkuN2XsP4Ol4rUonUD1&z=11&ll=' + coords);
+  }catch (err){
+    return;
+  }
 
 }
 
 
 async function initDoc() {
-  let data = (await(await fetch('/getSculpture?sculptureID='+sculptID)).json().catch(()=>{
-      return;
-  })).data;
-  document.getElementById('sculptTitle').innerHTML = data[0].Title;
-  document.getElementById('sculptText').innerHTML = data[0].Description;
-  document.getElementById('trailD').setAttribute('href', './trailDocs/Trail'+data[0].TrailID+'SculptureTrail.pdf');
-  document.getElementById('heritageD').setAttribute('href', './trailDocs/Trail'+data[0].TrailID+'HeritageTrail.pdf');
-  // add phto for sidebar
-  initArtist(data[0].ArtistID);
-  initTrailInfo(data[0].TrailID);
-  initMapInfo(data[0].TrailID);
-  document.getElementById('passButton').setAttribute('href', '/index.html?sculptureID='+sculptID);
+  try {
+    let resp = await fetch('/getSculpture?sculptureID='+sculptID);
+    if (!resp.ok) return;
+    let data = (await resp.json()).data;
+    document.getElementById('sculptTitle').innerHTML = data[0].Title;
+    document.getElementById('sculptText').innerHTML = data[0].Description;
+    document.getElementById('trailD').setAttribute('href', './trailDocs/Trail'+data[0].TrailID+'SculptureTrail.pdf');
+    document.getElementById('heritageD').setAttribute('href', './trailDocs/Trail'+data[0].TrailID+'HeritageTrail.pdf');
+    document.getElementById('sculptImg').setAttribute('src', './sculpturePhotos/Sculpture'+ sculptID+'.jpg');
+    initArtist(data[0].ArtistID);
+    initTrailInfo(data[0].TrailID);
+    initMapInfo(data[0].TrailID);
+    document.getElementById('passButton').setAttribute('href', '/index.html?sculptureID='+sculptID);
+  } catch (err){
+    return;
+  }
 
 }
