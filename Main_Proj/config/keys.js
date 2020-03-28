95a167cf2b99c196e93291493ae53768cb41bcd4
Ryan <rlail1607@gmail.com>
2020-03-28 16:05:33 +0000

    default now localhost database

--- Main_Proj/config/keys.js
+++ Main_Proj/config/keys.js
@@ -5,9 +5,9 @@ module.exports = {
         clientID: '1036868376146-6e9lh6ttm3g9ovi3f3mkosdk0fh1mqgo.apps.googleusercontent.com'
     },
     mysql:{
-        host: 'myeusql.dur.ac.uk', 
-        user: 'pvzm85', 
-        password: 'tues2day',
-        database: 'Cpvzm85_RTR'
+        host: 'localhost', // myeusql.dur.ac.uk
+        user: 'root', // pvzm85
+        password: '', // tues2day
+        database: 'Cpvzm85_RTR' // Cpvzm85_RTR
     }
 };
